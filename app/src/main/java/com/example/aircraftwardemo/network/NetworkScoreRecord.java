46d31d92e09481283206e7beee8f6d70f914d07d
有朝宿云 <2263041350@qq.com>
2026-03-19 13:20:01 +0800

    实现本地ScoreRecord的网络转换

--- app/src/main/java/com/example/aircraftwardemo/network/NetworkScoreRecord.java
+++ app/src/main/java/com/example/aircraftwardemo/network/NetworkScoreRecord.java
@@ -1,4 +1,36 @@
 package com.example.aircraftwardemo.network;
 
+import com.example.aircraftwardemo.data.ScoreRecord;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * 用于网络传输的分数记录
+ */
 public class NetworkScoreRecord {
+    @SerializedName("player_name")
+    private String playerName;
+
+    @SerializedName("score")
+    private int score;
+
+    @SerializedName("date_time")
+    private String dateTime;
+
+    public NetworkScoreRecord(String playerName, int score, String dateTime) {
+        this.playerName = playerName;
+        this.score = score;
+        this.dateTime = dateTime;
+    }
+
+    // 从本地ScoreRecord转换
+    public NetworkScoreRecord(ScoreRecord scoreRecord) {
+        this.playerName = scoreRecord.getName();
+        this.score = scoreRecord.getScore();
+        this.dateTime = scoreRecord.getDate();
+    }
+
+    // Getters
+    public String getPlayerName() { return playerName; }
+    public int getScore() { return score; }
+    public String getDateTime() { return dateTime; }
 }
---
+++ app/src/main/java/com/example/aircraftwardemo/network/ScoreNetworkManager.java
@@ -0,0 +1,4 @@
+package com.example.aircraftwardemo.network;
+
+public class ScoreNetworkManager {
+}
